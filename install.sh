8202252c05aeaf08976157fd95425d54a091b5c7
breeze <129418279+breeze-86@users.noreply.github.com>
2024-06-10 17:27:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,13 +10,13 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # Step 1: Install cpulimit tool
-echo "\e[1m[1/7]Installing cpulimit...\e[0m"
+echo -e "\e[1m[1/7]Installing cpulimit...\e[0m"
 apt-get update -y
 apt-get install -y cpulimit
 echo ""
 
 # Step 2: Configure Linux Network Device Settings
-echo "\e[1m[2/7]Configuring network device settings...\e[0m"
+echo -e "\e[1m[2/7]Configuring network device settings...\e[0m"
 # Check if net.core.rmem_max or net.core.wmem_max settings already exist
 if ! grep -q "^net\.core\.rmem_max=" /etc/sysctl.conf || ! grep -q "^net\.core\.wmem_max=" /etc/sysctl.conf; then
     # If they do not exist, add the following settings
@@ -34,7 +34,7 @@ echo ""
 
 
 # Step 3: Download the source code
-echo "\e[1m[3/7]Downloading the source code...\e[0m"
+echo -e "\e[1m[3/7]Downloading the source code...\e[0m"
 mkdir -p /opt && cd /opt
 git clone https://source.quilibrium.com/quilibrium/ceremonyclient.git
 
@@ -45,7 +45,7 @@ git checkout -b release origin/release
 echo ""
 
 # Step 4: Setup logrotate for log management
-echo "\e[1m[4/7]Setting up logrotate...\e[0m"
+echo -e "\e[1m[4/7]Setting up logrotate...\e[0m"
 tee /etc/logrotate.d/ceremonyclient_node_logrotate > /dev/null <<EOL
 ${ROOT_DIR}/node/logs/sys.log {
     size 500M
@@ -60,7 +60,7 @@ EOL
 echo ""
 
 # Step 5: Start the node
-echo "\e[1m[5/7]Starting the node...\e[0m"
+echo -e "\e[1m[5/7]Starting the node...\e[0m"
 cd ${ROOT_DIR}/node
 mkdir -p ${ROOT_DIR}/node/logs
 
@@ -83,7 +83,7 @@ echo ""
 # The following steps are for manual configuration after node setup.
 
 # Step 6: Modify the configuration file manually if necessary
-echo "\e[1m[6/7]Please modify the configuration file at ${ROOT_DIR}/node/.config/config.yml with the following settings\e[0m"
+echo -e "\e[1m[6/7]Please modify the configuration file at ${ROOT_DIR}/node/.config/config.yml with the following settings\e[0m"
 echo "To enable gRPC:"
 new_listenGrpcMultiaddr="/ip4/127.0.0.1/tcp/8337"
 echo "listenGrpcMultiaddr: \"${new_listenGrpcMultiaddr}\""
