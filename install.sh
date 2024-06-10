e93c6375e718ad9b6e807d15d698dc0da18de4da
breeze <129418279+breeze-86@users.noreply.github.com>
2024-06-10 16:41:26 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+ROOT_DIR="/opt/ceremonyclient"
+BAK_DIR="/data/quilibrium/bak"
+CONFIG_FILE_DIR="${ROOT_DIR}/node/.config/config.yml"
+
 if [ "$EUID" -ne 0 ]; then
   echo "Please use sudo to run this script."
   exit
@@ -9,6 +13,7 @@ fi
 echo "Installing cpulimit..."
 apt-get update -y
 apt-get install -y cpulimit
+echo ""
 
 # Step 2: Configure Linux Network Device Settings
 echo "Configuring network device settings..."
@@ -21,39 +26,12 @@ EOL
 # Apply the new sysctl settings
 echo "Applying sysctl settings..."
 sysctl -p
+echo ""
 
-# Step 3: Download the source code
-echo "Downloading the source code..."
-mkdir -p /opt
-cd /opt
-git clone https://source.quilibrium.com/quilibrium/ceremonyclient.git
-cd /opt/ceremonyclient
-
-# Checkout the release branch
-echo "Switching to the release branch..."
-git checkout -b release origin/release
-
-# Step 4: Start the node
-echo "Starting the node..."
-cd /opt/ceremonyclient/node
-mkdir -p /opt/ceremonyclient/node/logs
-
-# Create the run script
-echo "Creating the run script..."
-cat <<EOL > run.sh
-nohup bash /opt/ceremonyclient/node/release_autorun.sh > /opt/ceremonyclient/node/logs/sys.log 2>&1 & disown
-EOL
-
-chmod +x run.sh 
-
-# Run the node
-echo "Running the node..."
-./run.sh
-
-# Step 5: Setup logrotate for log management
+# Step 3: Setup logrotate for log management
 echo "Setting up logrotate..."
 tee /etc/logrotate.d/ceremonyclient_node_logrotate > /dev/null <<EOL
-/opt/ceremonyclient/node/logs/sys.log {
+${ROOT_DIR}/node/logs/sys.log {
     size 500M
     create 0644 root root
     rotate 1
@@ -63,6 +41,37 @@ tee /etc/logrotate.d/ceremonyclient_node_logrotate > /dev/null <<EOL
     notifempty
 }
 EOL
+echo ""
+
+# Step 4: Download the source code
+echo "Downloading the source code..."
+mkdir -p ${ROOT_DIR}
+cd ${ROOT_DIR}
+git clone https://source.quilibrium.com/quilibrium/ceremonyclient.git
+cd ${ROOT_DIR}
+
+# Checkout the release branch
+echo "Switching to the release branch..."
+git checkout -b release origin/release
+echo ""
+
+# Step 5: Start the node
+echo "Starting the node..."
+cd ${ROOT_DIR}/node
+mkdir -p ${ROOT_DIR}/node/logs
+
+# Create the run script
+echo "Creating the run script..."
+cat <<EOL > run.sh
+nohup bash ${ROOT_DIR}/node/release_autorun.sh > ${ROOT_DIR}/node/logs/sys.log 2>&1 & disown
+EOL
+
+chmod +x run.sh 
+
+# Run the node
+echo "Running the node..."
+./run.sh
+echo ""
 
 echo "Setup complete. Your node should be running and logs will be rotated automatically."
 
@@ -70,12 +79,23 @@ echo "Setup complete. Your node should be running and logs will be rotated autom
 # The following steps are for manual configuration after node setup.
 
 # Step 6: Modify the configuration file manually if necessary
-echo "Please modify the configuration file at /opt/ceremonyclient/node/.config/config.yml with the following settings:"
+echo "Please modify the configuration file at ${ROOT_DIR}/node/.config/config.yml with the following settings:"
 echo "To enable gRPC:"
 echo "listenGrpcMultiaddr: \"/ip4/127.0.0.1/tcp/8337\""
+new_listenGrpcMultiaddr="/ip4/127.0.0.1/tcp/8337"
+sed -i.bak "s|listenGrpcMultiaddr:.*|listenGrpcMultiaddr: \"${new_listenGrpcMultiaddr}\"|" "${CONFIG_FILE_DIR}"
+echo ""
+
 echo "To enable stats collection:"
 echo "engine:"
 echo "  statsMultiaddr: \"/dns/stats.quilibrium.com/tcp/443\""
+new_statsMultiaddr="/dns/stats.quilibrium.com/tcp/443"
+sed -i -e "/statsMultiaddr:/s|statsMultiaddr:.*|statsMultiaddr: \"{$new_statsMultiaddr}\"|" "${CONFIG_FILE_DIR}"
+echo ""
+
+# bak config
+mkdir -p ${BAK_DIR}
+cp -rf ${ROOT_DIR}/node/.config ${BAK_DIR}
 
 # Step 7: Reboot the VPS
 echo "Please reboot your VPS to apply the changes."
