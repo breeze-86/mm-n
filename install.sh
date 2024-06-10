d7db00b8643e48aa53bc0e10dc7fe3097e80e0f9
breeze <129418279+breeze-86@users.noreply.github.com>
2024-06-10 16:45:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,13 +10,13 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # Step 1: Install cpulimit tool
-echo "Installing cpulimit..."
+echo "[1/7]Installing cpulimit..."
 apt-get update -y
 apt-get install -y cpulimit
 echo ""
 
 # Step 2: Configure Linux Network Device Settings
-echo "Configuring network device settings..."
+echo "[2/7]Configuring network device settings..."
 tee -a /etc/sysctl.conf > /dev/null <<EOL
 # Increase buffer sizes for better network performance
 net.core.rmem_max=600000000
@@ -29,7 +29,7 @@ sysctl -p
 echo ""
 
 # Step 3: Setup logrotate for log management
-echo "Setting up logrotate..."
+echo "[3/7]Setting up logrotate..."
 tee /etc/logrotate.d/ceremonyclient_node_logrotate > /dev/null <<EOL
 ${ROOT_DIR}/node/logs/sys.log {
     size 500M
@@ -44,7 +44,7 @@ EOL
 echo ""
 
 # Step 4: Download the source code
-echo "Downloading the source code..."
+echo "[4/7]Downloading the source code..."
 mkdir -p ${ROOT_DIR}
 cd ${ROOT_DIR}
 git clone https://source.quilibrium.com/quilibrium/ceremonyclient.git
@@ -56,7 +56,7 @@ git checkout -b release origin/release
 echo ""
 
 # Step 5: Start the node
-echo "Starting the node..."
+echo "[5/7]Starting the node..."
 cd ${ROOT_DIR}/node
 mkdir -p ${ROOT_DIR}/node/logs
 
@@ -79,7 +79,7 @@ echo "Setup complete. Your node should be running and logs will be rotated autom
 # The following steps are for manual configuration after node setup.
 
 # Step 6: Modify the configuration file manually if necessary
-echo "Please modify the configuration file at ${ROOT_DIR}/node/.config/config.yml with the following settings:"
+echo "[6/7]Please modify the configuration file at ${ROOT_DIR}/node/.config/config.yml with the following settings:"
 echo "To enable gRPC:"
 echo "listenGrpcMultiaddr: \"/ip4/127.0.0.1/tcp/8337\""
 new_listenGrpcMultiaddr="/ip4/127.0.0.1/tcp/8337"
@@ -98,4 +98,4 @@ mkdir -p ${BAK_DIR}
 cp -rf ${ROOT_DIR}/node/.config ${BAK_DIR}
 
 # Step 7: Reboot the VPS
-echo "Please reboot your VPS to apply the changes."
+echo "[7/7]Please reboot your VPS to apply the changes."
