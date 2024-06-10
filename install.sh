756531af26e2ee55bfbd31a2d29cb73d1f577c3f
breeze <129418279+breeze-86@users.noreply.github.com>
2024-06-11 02:24:02 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,19 +4,21 @@ ROOT_DIR="/opt/ceremonyclient"
 BAK_DIR="/data/quilibrium/bak"
 CONFIG_FILE_DIR="${ROOT_DIR}/node/.config/config.yml"
 
+echo ""
+
 if [ "$EUID" -ne 0 ]; then
   echo "Please use sudo to run this script."
   exit
 fi
 
 # Step 1: Install cpulimit tool
-echo -e "\e[1m[1/7]Installing cpulimit...\e[0m"
+echo -e "\e[1m■■ [1/6]Installing cpulimit ■■\e[0m"
 apt-get update -y
 apt-get install -y cpulimit
 echo ""
 
 # Step 2: Configure Linux Network Device Settings
-echo -e "\e[1m[2/7]Configuring network device settings...\e[0m"
+echo -e "\e[1m■■ [2/6]Configuring network device settings ■■\e[0m"
 # Check if net.core.rmem_max or net.core.wmem_max settings already exist
 if ! grep -q "^net\.core\.rmem_max=" /etc/sysctl.conf || ! grep -q "^net\.core\.wmem_max=" /etc/sysctl.conf; then
     # If they do not exist, add the following settings
@@ -34,7 +36,7 @@ echo ""
 
 
 # Step 3: Download the source code
-echo -e "\e[1m[3/7]Downloading the source code...\e[0m"
+echo -e "\e[1m■■ [3/6]Downloading the source code ■■\e[0m"
 mkdir -p /opt && cd /opt
 git clone https://source.quilibrium.com/quilibrium/ceremonyclient.git
 
@@ -44,23 +46,8 @@ cd ${ROOT_DIR}
 git checkout -b release origin/release
 echo ""
 
-# Step 4: Setup logrotate for log management
-echo -e "\e[1m[4/7]Setting up logrotate...\e[0m"
-tee /etc/logrotate.d/ceremonyclient_node_logrotate > /dev/null <<EOL
-${ROOT_DIR}/node/logs/sys.log {
-    size 500M
-    create 0644 root root
-    rotate 1
-    compress
-    delaycompress
-    missingok
-    notifempty
-}
-EOL
-echo ""
-
-# Step 5: Start the node
-echo -e "\e[1m[5/7]Starting the node...\e[0m"
+# Step 4: Start the node
+echo -e "\e[1m■■ [4/6]Starting the node ■■\e[0m"
 echo "Create the service unit file..."
 # Create the service unit file
 tee /etc/systemd/system/ceremonyclient.service > /dev/null <<EOF
@@ -91,8 +78,8 @@ echo ""
 # Note: Configuration file modifications should be done after the node has started and created the config file.
 # The following steps are for manual configuration after node setup.
 
-# Step 6: Modify the configuration file manually if necessary
-echo -e "\e[1m[6/7]Please modify the configuration file at ${ROOT_DIR}/node/.config/config.yml with the following settings\e[0m"
+# Step 5: Modify the configuration file manually if necessary
+echo -e "\e[1m■■ [5/6]Please modify the configuration file at ${ROOT_DIR}/node/.config/config.yml with the following settings ■■\e[0m"
 echo "To enable gRPC:"
 new_listenGrpcMultiaddr="/ip4/127.0.0.1/tcp/8337"
 echo "listenGrpcMultiaddr: \"${new_listenGrpcMultiaddr}\""
@@ -110,5 +97,5 @@ echo ""
 mkdir -p ${BAK_DIR}
 cp -rf ${ROOT_DIR}/node/.config ${BAK_DIR}
 
-# Step 7: Reboot the VPS. Increase buffer sizes for better network performance 
-echo -e "\e[1;34m[7/7] Please < reboot > your VPS to apply the changes.\e[0m"
+# Step 6: Reboot the VPS. Increase buffer sizes for better network performance 
+echo -e "\e[1;34m■■ [6/6] Please < reboot > your VPS to apply the changes. ■■\e[0m"
